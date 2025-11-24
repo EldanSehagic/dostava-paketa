5b92f6a0c1c15ecdf80644aa1313bfb1aef0119f
Eldan <eldan.sehagic.23@size.ba>
2025-11-24 12:51:12 +0100

    Dodan dependecies za JUnit testove u pom.xml,potreban bio reload projekta,importovan JUnit test u src/test/java/PosiljkaTest i dodan test void testKreiranjePaketa()

--- src/test/java/ba/dostava/PosiljkaTest.java
+++ src/test/java/ba/dostava/PosiljkaTest.java
@@ -1,4 +1,14 @@
 package ba.dostava;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PosiljkaTest {
+    @Test
+    void testKreiranjPaketa(){
+        Paket p = new Paket("P001",10,50);
+        assertEquals("P001",p.getTrackingNumber());
+        assertEquals(10,p.getTezinaKG());
+        assertEquals(50,p.getTezinaKG());
+    }
 }
