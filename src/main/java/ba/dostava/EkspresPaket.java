778a9cddbab532dd59e9ffeccef6d6c6ead31f05
Eldan <eldan.sehagic.23@size.ba>
2025-11-24 12:08:06 +0100

    Dodan konstruktro u EkspresPaket

--- src/main/java/ba/dostava/EkspresPaket.java
+++ src/main/java/ba/dostava/EkspresPaket.java
@@ -1,4 +1,8 @@
 package ba.dostava;
 
-public class EkspresPaket {
+public class EkspresPaket extends Paket{
+    //napravljen konstruktor
+    public EkspresPaket(String trackingNumber, double tezinaKG, double udaljenostKm) {
+        super(trackingNumber, tezinaKG, udaljenostKm);
+    }
 }
