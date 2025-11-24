98a0e3450a52c1800c87e586d11c24980116dd51
Eldan <eldan.sehagic.23@size.ba>
2025-11-24 13:34:45 +0100

    Napravljena HashMap u Main

--- src/main/java/ba/dostava/Main.java
+++ src/main/java/ba/dostava/Main.java
@@ -1,4 +1,10 @@
 package ba.dostava;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+    public static void main(String[] args) {
+        Map<String,Posiljka> dostava = new HashMap<>();
+    }
 }
