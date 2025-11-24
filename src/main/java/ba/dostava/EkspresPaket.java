afca0d4c22b91f102ee926460073778d3caeda20
Eldan <eldan.sehagic.23@size.ba>
2025-11-24 12:11:28 +0100

    Override-ana metoda izracunajBaznuCijenu() i dodano *1.5 u EkspresPaket

--- src/main/java/ba/dostava/EkspresPaket.java
+++ src/main/java/ba/dostava/EkspresPaket.java
@@ -5,4 +5,10 @@ public class EkspresPaket extends Paket{
     public EkspresPaket(String trackingNumber, double tezinaKG, double udaljenostKm) {
         super(trackingNumber, tezinaKG, udaljenostKm);
     }
+
+    //override-ana metoda
+    @Override
+    public double izracunajBaznuCijenu() {
+        return super.izracunajBaznuCijenu()*1.5;
+    }
 }
