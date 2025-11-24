a21b5de10e6ecda0cdc834039fae89db8839fdb6
Eldan <eldan.sehagic.23@size.ba>
2025-11-24 12:56:17 +0100

    Popravljena greska u testu testKreiranjePaketa u assertEquals,nakon ovog ovaj test prolazi uredno

--- src/test/java/ba/dostava/PosiljkaTest.java
+++ src/test/java/ba/dostava/PosiljkaTest.java
@@ -9,6 +9,6 @@ public class PosiljkaTest {
         Paket p = new Paket("P001",10,50);
         assertEquals("P001",p.getTrackingNumber());
         assertEquals(10,p.getTezinaKG());
-        assertEquals(50,p.getTezinaKG());
+        assertEquals(50,p.getUdaljenostKm());
     }
 }
