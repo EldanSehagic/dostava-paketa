c68dd83bffa20556bef3bd595f5ff75fbc20f293
Eldan <eldan.sehagic.23@size.ba>
2025-11-24 13:37:24 +0100

    Dodana dva paketa  i dva ekspres paketa u Main

--- src/main/java/ba/dostava/Main.java
+++ src/main/java/ba/dostava/Main.java
@@ -6,5 +6,10 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         Map<String,Posiljka> dostava = new HashMap<>();
+
+        Paket p1 = new Paket("P001",10,50);
+        Paket p2 = new Paket("P002",20,20);
+        EkspresPaket ep1 = new EkspresPaket("E001",8,40);
+        EkspresPaket ep2 = new EkspresPaket("E002",2,20);
     }
 }
